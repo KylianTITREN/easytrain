5394bad7617fe601e070fe568d6424cd01f80c20
MartinMagnier <martin_mgnr@hotmail.com>
2018-04-10 12:48:28 +0200

    Slider des muscles dans exercices.

--- resources/views/exercices.blade.php
+++ resources/views/exercices.blade.php
@@ -7,9 +7,46 @@ $icons = 'comments';
 
 ?>
 
+<style>
+        html, body {
+                position: relative;
+                height: 100%;
+        }
+        body {
+                background: #eee;
+                font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
+                font-size: 14px;
+                color:#000;
+                margin: 0;
+                padding: 0;
+        }
+        .swiper-container {
+                width: 100%;
+                height: 100%;
+        }
+        .swiper-slide {
+                text-align: center;
+                font-size: 18px;
+                background: #fff;
+                /* Center slide text vertically */
+                display: -webkit-box;
+                display: -ms-flexbox;
+                display: -webkit-flex;
+                display: flex;
+                -webkit-box-pack: center;
+                -ms-flex-pack: center;
+                -webkit-justify-content: center;
+                justify-content: center;
+                -webkit-box-align: center;
+                -ms-flex-align: center;
+                -webkit-align-items: center;
+                align-items: center;
+        }
+</style>
+
 <span class="goBack">
         <a href="{{ URL::previous() }}"><i class="fa fa-angle-left" style="color:#7FED72;"></i></a>
-    </span>
+</span>
 
 @section('content')
 
@@ -18,15 +55,18 @@ $icons = 'comments';
                 <i class="fa fa-{{ $icons }}" aria-hidden="true"></i>
         </header>
 
-        <ul>
+        <div class="swiper-exercice">
+                <div class="swiper-wrapper">
+                        @foreach ($muscle->exercices as $e)
 
-                @foreach ($muscle->exercices as $e)
+                                <div class="swiper-slide">
+                                        <li><a href="#">{{$e->nom}}</a></li><br>
+                                </div>
 
-                        <li><a href="#">{{$e->nom}}</a></li><br>
+                        @endforeach
+                </div>
+        </div>
 
-
-                @endforeach
-        </ul>
 
         @include('layouts.footer')
 
--- resources/views/program.blade.php
+++ resources/views/program.blade.php
@@ -9,6 +9,42 @@ $icons = 'comments';
 
 @section('content')
 
+    <style>
+
+        .swiper-container {
+            width: 100%;
+            height: 100%;
+        }
+        .swiper-slide {
+            text-align: center;
+            font-size: 18px;
+            background: #F8F8F8;/
+            display: -webkit-box;
+            display: -ms-flexbox;
+            display: -webkit-flex;
+            display: flex;
+            -webkit-box-pack: center;
+            -ms-flex-pack: center;
+            -webkit-justify-content: center;
+            justify-content: center;
+            -webkit-box-align: center;
+            -ms-flex-align: center;
+            -webkit-align-items: center;
+            align-items: center;
+            box-shadow: 0px 1px 6px #C7C7C7;
+            border-radius: 50px;
+            height: 70px;
+            width: 70px!important;
+            margin: 0 4px;
+        }
+
+        .swiper-wrapper
+        {
+            margin-top: 10px;
+            margin-bottom: 20px
+        }
+    </style>
+
 <header>
     <form id="search" data-pjax>
         <input type="search" name="search" placeholder="&#xf002;&#32;&#32; Rechercher" required>
@@ -19,11 +55,27 @@ $icons = 'comments';
 
 <section class="programme">
 
-    <a href="{{ url('/muscles') }}" data-pjax>
-        <div class="exercice">
-            <strong>EXERCICES</strong>
+    <div class="programme-bloc">
+
+        <h3>choisir son exercice</h3>
+
+        <div class="swiper-container">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide"><a href=""><img src="img/bras.png" alt="bras"></a></div>
+                <div class="swiper-slide"><a href=""><img src="img/bras.png" alt="bras"></a></div>
+                <div class="swiper-slide"><a href=""><img src="img/bras.png" alt="bras"></a></div>
+                <div class="swiper-slide"><a href=""><img src="img/bras.png" alt="bras"></a></div>
+                <div class="swiper-slide"><a href=""><img src="img/bras.png" alt="bras"></a></div>
+            </div>
         </div>
-    </a>
+
+    </div>
+
+    <div class="programme-bloc">
+
+        <h3>programme personnalisé</h3>
+
+    </div>
 
     <br>
 
